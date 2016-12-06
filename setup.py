3e2e27033737f64baab2162aab2ccdfaa8f8a234
slackmart <slackmart@gmail.com>
2016-12-06 08:49:06 -0600

    Restore setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=['six'],
-    # cmdclass={
-    #     'develop': DevelopCmd
-    # },
+    cmdclass={
+        'develop': DevelopCmd
+    },
 )
